4cb79804c044fe34547885739ac89b00257a673d
ᔕᑌᗪOᒪᒪ <ko502804@naver.com>
2022-06-13 15:06:12 +0900

    Add files via upload

---
+++ employeeModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const {model} = require("mongoose");
+const Schema = mongoose.Schema
+
+const employeeSchema = new Schema({
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    phone: {
+        type: String
+    }
+},
+{timestamps : true})
+
+const Employee = mongoose.model('Employee', employeeSchema)
+module.exports = Employee;
