981b04343899a928fc68bc26ad43ac09ec5f9272
ᔕᑌᗪOᒪᒪ <ko502804@naver.com>
2022-06-13 14:30:36 +0900

    DB연결

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 const app = express();
+
+mongoose.connect(process.env.DB_CONNECTION , {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection;
+db.on('error', function (err){
+    console.log(err)
+})
+
+db.once('open', function (){
+    console.log('디비 연결');
+})
 
 // morgan HTTP request 기록해주는 미들웨어
 app.use(morgan('dev'))
 
-const port = process.env.PORT || 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT);
